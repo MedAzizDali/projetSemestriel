8da5cd14b77fd5534033ee080804cec760aaa16f
Baananou Med Yessine <62482762+Baananou@users.noreply.github.com>
2021-12-30 15:07:22 +0100

    hellllooo

--- index.php
+++ index.php
@@ -36,7 +36,6 @@
             <li><a href="#about" data-after="About">About</a></li>
             <li><a href="#contact" data-after="Contact">Contact</a></li>
             <li><a style="font-family: noto;"> Username: <?php echo $row["username"]; ?></a></li>
-            <li><a > </a></li>
           </ul>
         </div>
       </div>
